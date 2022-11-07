56e7ab20264c86a4eb52df49039d0677409f78f1
wangqingbo <wangqingbo@huimin100.cn>
2022-11-07 18:06:07 +0800

    注册中心搭建

--- eureka/src/main/java/com/open/bright/eureka/ServletInitializer.java
+++ eureka/src/main/java/com/open/bright/eureka/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.open.bright.gateway;
+package com.open.bright.eureka;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(GatewayApplication.class);
+		return application.sources(EurekaApplication.class);
 	}
 
 }
---
+++ form/src/main/java/com/open/bright/form/FormApplication.java
@@ -0,0 +1,16 @@
+package com.open.bright.form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+
+@SpringBootApplication
+public class FormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormApplication.class, args);
+	}
+
+}
