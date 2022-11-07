b7e9dc60e838abec9d95c227192510c8845a2cfa
wangqingbo <wangqingbo@huimin100.cn>
2022-11-07 15:21:21 +0800

    网关先行

---
+++ gateway/src/main/java/com/open/bright/gateway/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.open.bright.gateway;
+
+import com.open.bright.gateway.GatewayApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GatewayApplication.class);
+	}
+
+}
