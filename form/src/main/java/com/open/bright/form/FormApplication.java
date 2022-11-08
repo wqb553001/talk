81ea237b24343e933706e6422c0af282dcbb5b9d
wangqingbo <wangqingbo@huimin100.cn>
2022-11-08 17:19:50 +0800

    1.服务监控；2.swagger

--- form/src/main/java/com/open/bright/form/FormApplication.java
+++ form/src/main/java/com/open/bright/form/FormApplication.java
@@ -1,16 +1,25 @@
 package com.open.bright.form;
 
+import io.swagger.annotations.Api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
-@SpringBootApplication
+@EnableSwagger2
+@SpringBootApplication(scanBasePackages={"com.open.bright"})
+@ComponentScan(basePackages = {"com.open.bright"})
 public class FormApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FormApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
 }
